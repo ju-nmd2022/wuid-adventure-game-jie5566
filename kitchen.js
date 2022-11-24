396390485ab3409efb100fdc48533e98f74580b4
Jie C <117114870+jie5566@users.noreply.github.com>
2022-11-24 16:39:45 +0100

    Third edtion
    add if else

--- kitchen.js
+++ kitchen.js
@@ -2,16 +2,41 @@ let item1 = document.getElementById("cheatsheet")
 let item2 = document.getElementById("book")
 let item3 = document.getElementById("coffee")
 let item4 = document.getElementById("beer")
+let chooseCoffee = document.getElementById("chooseCoffee")
+let chooseBeer = document.getElementById("chooseBeer")
+let buttonCoffee = document.getElementById("btncoffee")
+let buttonBeer = document.getElementById("btnbeer")
 
-item1.style.visibility= "visible"
-item2.style.visibility= "visible"
+
 
 chooseCoffee.addEventListener("click", function(){
     item3.style.visibility= "visible"
+    buttonCoffee.style.visibility= "visible"
+    sessionStorage.setItem("saveItem3", "itemClick")
      })
     
      chooseBeer.addEventListener("click", function(){
     item4.style.visibility= "visible"
+    buttonBeer.style.visibility= "visible"
+    sessionStorage.setItem("saveItem4", "itemClick")
     })
 
-    // if I choose coffee, I cannot click beer
+
+    if(sessionStorage.getItem("saveItem1") != null){
+        item1.style.visibility= "visible"
+    }
+    
+    if(sessionStorage.getItem("saveItem2") != null){
+        item2.style.visibility= "visible"
+    }
+    
+      if (sessionStorage.getItem("saveItem2") != null && sessionStorage.getItem("saveItem1") != null) 
+      {
+        middleButton.style.visibility= "visible"
+    } 
+    
+
+
+
+
+    
